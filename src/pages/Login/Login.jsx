7ea8db1ab625640d39647c2a21841bcfa2812d3f
tarikul-islam <tariikul12@gmail.com>
2023-05-18 09:03:36 +0600

    Banner section Done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,52 @@
-
+import login from '../../../public/login.json'
+import Lottie from "lottie-react";
+import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+      const [data, setData] = useState("");
+
+      console.log(data)
     return (
-        <div>
-            <h1>login</h1>
+        <div className="hero min-h-screen md:h-72 bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+          <Lottie animationData={login} loop={true} />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl md:h-[500px] bg-base-100">
+            <div className="card-body">
+                <h1 className='text-center text-4xl font-bold'>Login Here</h1>
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+               
+                <input {...register("email")} type="email" placeholder="Enter Your Email" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input {...register("password")} type="password" placeholder="Enter Your Password" className="input input-bordered" />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control ">
+                <button className="btn   bg-cyan-700 hover:bg-cyan-400 text-white">Login</button>
+              </div>
+              </form>
+              <div className="divider">OR</div>
+              <div>
+                <FaGoogle className='ms-32  h-10 w-10 text-cyan-700 hover:text-cyan-400 ' />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/pages/Shared/Header/Banner/Banner.jsx
+++ src/pages/Shared/Header/Banner/Banner.jsx
@@ -13,37 +13,101 @@ import "./banner.css";
 import { EffectCreative } from "swiper";
 
 export default function Banner() {
-  return (
-    <>
-      <Swiper
-        grabCursor={true}
-        effect={"creative"}
-        creativeEffect={{
-          prev: {
-            shadow: true,
-            origin: "left center",
-            translate: ["-5%", 0, -200],
-            rotate: [0, 100, 0],
-          },
-          next: {
-            origin: "right center",
-            translate: ["5%", 0, -200],
-            rotate: [0, -100, 0],
-          },
-        }}
-        modules={[EffectCreative]}
-        className="mySwiper6"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
-  );
+    return (
+        <>
+            <Swiper
+                grabCursor={true}
+                effect={"creative"}
+                creativeEffect={{
+                    prev: {
+                        shadow: true,
+                        origin: "left center",
+                        translate: ["-5%", 0, -200],
+                        rotate: [0, 100, 0],
+                    },
+                    next: {
+                        origin: "right center",
+                        translate: ["5%", 0, -200],
+                        rotate: [0, -100, 0],
+                    },
+                }}
+                modules={[EffectCreative]}
+                className="mySwiper6"
+            >
+                <SwiperSlide>
+                    <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn  bg-cyan-700 hover:bg-cyan-400 text-white flex justify-center">Shop Now</button>
+                    </div>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className=" md:w-1/3 md:ms-auto mt-auto p-10 md:mt-0">
+                        <h4 className="text-2xl mb-3 font-bold">Discover endless creativity</h4>
+                        <h1 className="text-4xl font-bold text-cyan-200">Remodel the way they build castles</h1>
+                        <p className="font-bold my-3">Innovative eco-friendly builders kit</p>
+                        <button className="btn btn-accent text-white flex justify-center">Shop Now</button>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
 }
